4de3573018e6995a398cec277139f0f9ab455a76
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-10-17 16:13:18 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Munia Sircar, open-sourced on{" "}
-        <a
-          href="https://github.com/Munia-Sircar/react-weather-app"
-          target="_blank"
-          rel="noreferrer">
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Munia Sircar, open-sourced on{" "}
+          <a
+            href="https://github.com/Munia-Sircar/react-weather-app"
+            target="_blank"
+            rel="noreferrer">
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
