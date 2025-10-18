992417b70ae33eb4b8b35d8e1dfe973bd2231cf4
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-10-18 12:27:10 -0500

    API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,49 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="weather">
-      <div className="row">
-        <div className="col-9">
-          <input
-            type="search"
-            placeholder="Enter a city..."
-            className="form-control"
-            autoFocus
-          />
-        </div>
+  let [ready, setReady] = useState(false);
+  let [weather, setWeather] = useState({});
 
-        <div className="col-3">
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-primary w-100"
-          />
+  function showWeather(response) {
+    console.log(response);
+    setWeather({
+      temp: response.data.temperature.current,
+      city: response.data.city,
+      description: response.data.condition.description,
+      iconUrl: response.data.condition.icon_url,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      time: response.data.time,
+    });
+
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="weather">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus
+            />
+          </div>
+
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+        <h1>{weather.city}</h1>
+        <ul className="current-info">
+          <li>Friday 5:00pm</li>
+          <li className="text-capitalize">{weather.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <img src={weather.iconUrl} alt="icon-image" />
+            <span className="temperature">{Math.round(weather.temp)}</span>
+            <span className="unit">°C</span>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weather.humidity}%</li>
+              <li>Wind: {Math.round(weather.wind)}km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-      <h1>Rome</h1>
-      <ul className="current-info">
-        <li>Friday 5:00pm</li>
-        <li>Crescent moon</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://www.gstatic.com/weather/conditions/v1/svg/clear_night_light.svg"
-            alt="icon-image"
-          />
-          <span className="temperature">6</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 8km/h</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Dhaka&key=28d0bff0ffaa52b09e33da6etode543b&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+    return "Loading..";
+  }
 }
