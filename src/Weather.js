df7ada52e1c83b8f17b0b7be7b82afa90ed19c45
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-10-21 18:30:51 -0500

    built search engine functionality

--- src/Weather.js
+++ src/Weather.js
@@ -7,7 +7,7 @@ import WeatherInfo from "./WeatherInfo";
 export default function Weather() {
   let [ready, setReady] = useState(false);
   let [weather, setWeather] = useState({});
-
+  let [city, setCity] = useState("Winnipeg");
   function showWeather(response) {
     console.log(response);
     setWeather({
@@ -22,10 +22,24 @@ export default function Weather() {
     setReady(true);
   }
 
+  function searchCity() {
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=28d0bff0ffaa52b09e33da6etode543b&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    searchCity();
+  }
+
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
+
   if (ready) {
     return (
       <div className="weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -33,6 +47,7 @@ export default function Weather() {
                 placeholder="Enter a city..."
                 className="form-control"
                 autoFocus
+                onChange={handleChange}
               />
             </div>
 
@@ -49,8 +64,7 @@ export default function Weather() {
       </div>
     );
   } else {
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Winnipeg&key=28d0bff0ffaa52b09e33da6etode543b&units=metric`;
-    axios.get(apiUrl).then(showWeather);
+    searchCity();
     return "Loading..";
   }
 }
