92f3d734788bb3771673930bedf8a0805d93f959
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-10-28 18:56:39 -0500

    Added forecast component and API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast() {
+  function showResponse(response) {
+    console.log(response);
+  }
+
+  let city = "Tokyo";
+  let apiKey = "28d0bff0ffaa52b09e33da6etode543b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="forecast-day">Thu</div>
