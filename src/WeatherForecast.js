7c88d302b59dc1ac08746fdb1eaf0689aafb0ff8
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-10-28 20:14:02 -0500

    Added real data to forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast() {
-  function showResponse(response) {}
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
-  let city = "Tokyo";
-  let apiKey = "28d0bff0ffaa52b09e33da6etode543b";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  function showResponse(response) {
+    setLoaded(true);
+    setForecast(response.data.daily);
+  }
 
-  axios.get(apiUrl).then(showResponse);
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="forecast-day">{forecast[0].time}</div>
 
-  return (
-    <div className="WeatherForecast">
-      <div className="forecast-day">Thu</div>
-      <div className="forecast-icon">🌦️</div>
-      <div className="forecast-temp">
-        <span className="forecast-temp-max">19</span>
-        <span className="forecast-temp-min">10</span>
+        <div className="forecast-icon">
+          <img src={forecast[0].condition.icon_url} alt="icon-image" />
+        </div>
+        <div className="forecast-temp">
+          <span className="forecast-temp-max">
+            {Math.round(forecast[0].temperature.maximum)}°
+          </span>
+          <span className="forecast-temp-min">
+            {Math.round(forecast[0].temperature.minimum)}°
+          </span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = "Tokyo";
+    let apiKey = "28d0bff0ffaa52b09e33da6etode543b";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(showResponse);
+
+    return null;
+  }
 }
